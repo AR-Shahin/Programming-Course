f15b9aaf2291f7ff36dc7dfce83f433342b52a6e
AR-Shahin <mdshahinmije96@gmail.com>
2022-02-15 22:08:08 +0600

    radius problem

--- Basic/problem.cpp
+++ Basic/problem.cpp
@@ -3,12 +3,13 @@ using namespace std;
 #define PI 3.1416
 int main()
 {
-    float r;
+    float r, c;
     float area;
     cout << "Enter Radius : ";
     cin >> r;
     area = PI * (r * r);
     cout << "Area of Circle " << area;
 
+    float f = (c * (9 / 2.0) + 32);
     return 0;
 }
