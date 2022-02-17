04edab1fba05d7c51fcdcaf26751482d45fa88fa
AR-Shahin <mdshahinmije96@gmail.com>
2022-02-17 21:28:35 +0600

    basic salary

--- Basic/problem.cpp
+++ Basic/problem.cpp
@@ -3,14 +3,6 @@ using namespace std;
 #define PI 3.1416
 int main()
 {
-    int a, b;
-    cin >> a >> b;
 
-    int temp;
-    temp = a; // a er data
-    a = b;    // b er data
-    b = temp; // a er data
-
-    cout << "A = " << a << " B = " << b;
     return 0;
 }
--- Basic/test.cpp
+++ Basic/test.cpp
@@ -4,56 +4,14 @@ using namespace std;
 
 int main()
 {
-    // int variableName, v1, v2, v3;
-    // float f1;
-    // double d1;
-    // const float PI = 3.1416;
-    // bool b1;
-    // v1 = 10;
-    // v1 = 100;
-    // cout << "Enter A value : " << endl;
-    // cin >> f1;
-    // cout << f1;
+    float salary, house_rent, medical, technical, gross;
 
-    // string s1;
-    // s1 = "Shahin";
-    // cout << s1;
+    cin >> salary;
 
-    // v1 = 10;
-    // v2 = 3;
-
-    // cout << "Addition : " << v1 + v2 + 10 << endl;
-    // cout << "Subration : " << v1 - v2 << endl;
-    // cout << "Multripication : " << v1 * v2 << endl;
-    // cout << "Division : " << v1 / v2 << endl;
-    // cout << "Modules : " << v1 % v2 << endl;
-    // cout << "Addition : " << v1 + v2 + 10;
-
-    // v1 = v1 + 5
-    // v1 += 5;
-    // v1 = v1 - 5;
-    // v1 -= 5;
-    // v1 = v1 + 1;
-    // v1++;
-    // v1 = v1 - 1;
-    // v1--;
-
-    // cout << "PreDecrement " << --v1 << endl;
-    // cout << "PostDecrement " << v1-- << endl;
-    // cout << "After " << v1 << endl;
-    /*
-    cout<<v1++<<endl; // 10
-    cout <<v1; // 11*/
-    // cout << v1;
-
-    int a, b;
-
-    cout << "Enter Values: ";
-    cin >> a >> b;
-
-    cout << "Addition : " << a + b << endl;
-    cout << "Substraction : " << a - b << endl;
-    cout << "Multipicaion : " << a * b << endl;
-    cout << "Division : " << a / b << endl;
+    house_rent = salary * 0.3;
+    medical = salary * 0.05;
+    technical = 1000;
+    gross = salary + house_rent + medical + technical;
+    cout << "Gross Salary : " << gross;
     return 0;
 }
