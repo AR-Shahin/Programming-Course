9bb8a50c6131e07e34fef147c2f70202935234c0
AR-Shahin <mdshahinmije96@gmail.com>
2022-02-20 09:27:08 +0600

    grade

--- Basic/problem.cpp
+++ Basic/problem.cpp
@@ -3,16 +3,47 @@ using namespace std;
 #define PI 3.1416
 int main()
 {
-    int year;
-    cin >> year;
-
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    int marks;
+    cin >> marks;
+    if (marks >= 80 && marks <= 100)
     {
-        cout << "Leaf Year";
+        cout << "A+";
     }
-    else
+    else if (marks >= 75 && marks <= 79)
     {
-        cout << "Not Leaf Year";
+        cout << "A";
+    }
+    else if (marks >= 70 && marks <= 74)
+    {
+        cout << "A-";
+    }
+    else if (marks >= 65 && marks <= 69)
+    {
+        cout << "B+";
+    }
+    else if (marks >= 60 && marks <= 64)
+    {
+        cout << "B";
+    }
+    else if (marks >= 55 && marks <= 59)
+    {
+        cout << "B-";
+    }
+    else if (marks >= 50 && marks <= 54)
+    {
+        cout << "C+";
+    }
+    else if (marks >= 45 && marks <= 49)
+    {
+        cout << "C";
+    }
+    else if (marks >= 40 && marks <= 44)
+    {
+        cout << "D";
+    }
+    else if (marks >= 0 && marks <= 99)
+    {
+        cout << "F";
     }
     return 0;
 }
