392c7f8d81586397b00b5a47ec580f88d500ffd6
AR-Shahin <mdshahinmije96@gmail.com>
2022-02-20 09:36:13 +0600

    vowel consonennt

--- Basic/problem.cpp
+++ Basic/problem.cpp
@@ -3,47 +3,15 @@ using namespace std;
 #define PI 3.1416
 int main()
 {
-    int marks;
-    cin >> marks;
-    if (marks >= 80 && marks <= 100)
+    char ch;
+    cin >> ch;
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
     {
-        cout << "A+";
+        cout << "Vowel";
     }
-    else if (marks >= 75 && marks <= 79)
+    else
     {
-        cout << "A";
-    }
-    else if (marks >= 70 && marks <= 74)
-    {
-        cout << "A-";
-    }
-    else if (marks >= 65 && marks <= 69)
-    {
-        cout << "B+";
-    }
-    else if (marks >= 60 && marks <= 64)
-    {
-        cout << "B";
-    }
-    else if (marks >= 55 && marks <= 59)
-    {
-        cout << "B-";
-    }
-    else if (marks >= 50 && marks <= 54)
-    {
-        cout << "C+";
-    }
-    else if (marks >= 45 && marks <= 49)
-    {
-        cout << "C";
-    }
-    else if (marks >= 40 && marks <= 44)
-    {
-        cout << "D";
-    }
-    else if (marks >= 0 && marks <= 99)
-    {
-        cout << "F";
+        cout << "Consonant";
     }
     return 0;
 }
