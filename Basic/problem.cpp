ef9358206825d28c52a364fc37103582216d4b30
AR-Shahin <mdshahinmije96@gmail.com>
2022-03-10 22:17:07 +0600

    perfect number

--- Basic/problem.cpp
+++ Basic/problem.cpp
@@ -26,8 +26,32 @@ void fibonacci(int n)
     }
 }
 
+bool isPerfect(int n)
+{
+    int sum = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            sum += i;
+        }
+    }
+
+    if (sum == n)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main()
 {
-    fibonacci(8);
+    // fibonacci(8);
+
+    cout << isPerfect(5);
     return 0;
 }
