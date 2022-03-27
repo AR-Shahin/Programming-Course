c9b30d7f83b9dcd760705f57d619aa22cfe8af9d
AR-Shahin <mdshahinmije96@gmail.com>
2022-03-27 22:01:01 +0600

    level 0

---
+++ Basic/class.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+
+void shakib0()
+{
+
+    cout << "abc";
+}
+void shakib1()
+{
+
+    cout << "test";
+}
+int main()
+{
+    int TOSS;
+    cout << "Enter Number : "; // printf("ddd");
+    cin >> TOSS;               // scanf(%)
+
+    if (TOSS == 0)
+    {
+
+        // work
+        shakib0();
+    }
+    else if (TOSS == 1)
+    {
+        // work
+        shakib1();
+    }
+    return 0;
+}
