46a172086e48964df074114fc2fee850f95298c3
AR-Shahin <mdshahinmije96@gmail.com>
2022-09-20 21:52:43 +0600

    Stack implement

---
+++ DS/stack_array.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#define size 5
+using namespace std;
+
+int stack[size];
+int top = 0;
+
+void push(int val){
+    if(top < size){
+        stack[top] = val;
+        top ++;
+    }else{
+        cout<<"Stack is Full!\n";
+    }
+}
+
+void display(){
+    cout<<endl;
+    for(int i = top -1;i>=0;i--){
+        cout<<stack[i]<<" ";
+    }
+}
+
+int pop(){
+    int data = stack[top-1];
+    top --;
+    return data;
+}
+
+int peek(){
+    return stack[top-1];
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    // display();
+    // cout<<pop()<<endl;
+    // cout<<pop()<<endl;
+    // cout<<pop()<<endl;
+    cout<<peek();
+    return 0;
+}
